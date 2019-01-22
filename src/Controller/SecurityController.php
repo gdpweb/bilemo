ba914c97613c72659ecdbe7722b6736cf620f783
gdpweb <stephanebriere@gdpweb.fr>
2019-01-22 18:25:02 +0100

    add user

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $data = json_decode($request->getContent());
+        $user = new User();
+        $user->setUsername($data->username);
+        $user->setPassword($passwordEncoder->encodePassword($user, $data->password));
+        $user->setEmail($data->email);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return new Response('L\'utilisateur a bien été créé');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+        return $this->json(array(
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ));
+    }
+}
